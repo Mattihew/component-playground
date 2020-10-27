9ef814e7f801645244974d64960c3fbfa2521407
Matt Rayner <me@mattihew.com>
2020-10-27 10:33:34 +0000

    updated decorator to allow controls to not rerender

--- src/common/map/Map.stories.tsx
+++ src/common/map/Map.stories.tsx
@@ -1,25 +1,25 @@
-import React, {ComponentPropsWithoutRef} from "react";
+import React, { ComponentPropsWithoutRef } from "react";
 import { Meta, Story } from "@storybook/react/types-6-0";
 import { MapComp } from "./Map";
 
 export default {
   title: "common/Map",
   component: MapComp,
-  decorators: [(Story) => <div style={{height: 500}}><Story /></div>],
+  decorators: [(Story) => <div style={{ height: 500 }}>{Story()}</div>],
   argTypes: {
     default3D: {
       control: {
-        type: "boolean" 
-      }
-    }
-  }
+        type: "boolean",
+      },
+    },
+  },
 } as Meta;
 
 type Props = ComponentPropsWithoutRef<typeof MapComp>;
 
-const Template: Story<Props> = (args) => <MapComp {...args}/>;
+const Template: Story<Props> = (args) => <MapComp {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  default3D: true
-}
+  default3D: true,
+};
